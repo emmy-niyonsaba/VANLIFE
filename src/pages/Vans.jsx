c5d3bd9ccc542b8e88eeef525bcc453a08d7fdb0
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-02 19:45:15 +0200

    done withe headerof van section

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import VansLayout from '../layouts/VansLayout'
 
 const Vans = () => {
+    const categories = ["simple", "luxury", "rugged"]
     const [vans, setVans] = useState([])
     console.log(vans)
     useEffect(() => {
@@ -12,15 +13,27 @@ const Vans = () => {
             .then(data => setVans(data.vans))
     }, [])
     return (
+        <>
+            <h1 className=' text-5xl font-bold  m-10'>Explore our van options</h1>
+                <div className='flex gap-4 m-10'>
+                    {categories.map((category) => {
+                        return (
+                            <button key={category} className=' bg-[#f0ca9c] text-white px-4 py-2 rounded-md'>{category}</button>
+                        )
+                    })}
+                  <Link>  <h2 className=' font-bold text-2xl '>Clear Filter</h2></Link>
+                </div>
         <div className='flex flex-wrap gap-4 justify-center mt-10'>
             {
-                vans.map((van)=>{
+                vans.map((van) => {
                     return (
                         <VansLayout key={van.id} {...van} />
                     )
                 })
             }
         </div>
+        </>
+
     )
 }
 
