a82648845fa2ebbbfdd91cd167482cb20695bee6
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-03 09:12:19 +0200

    done with the NavLink instead of Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 function Navbar() {
   return (  
     <div className='flex items-center justify-between p-4 bg-[#FFF7ED] text-grey-100'>
-   <Link to='/'><h1 className=' text-[#000000] font-bold text-2xl'>#VANLIFE</h1></Link>
+   <NavLink to='/'><h1 className=' text-[#000000] font-bold text-2xl'>#VANLIFE</h1></NavLink>
     <div className=' flex gap-10 text-[#4D4D4D] '>
-        <Link to='/hosts' className=' hover:underline'>Hosts</Link>
-        <Link to='/about' className=' hover:underline'>About</Link>
-       <Link to='/vans' className=' hover:underline'>Vans</Link>
+       <NavLink
+  to="/hosts"
+  className={({ isActive }) =>
+    `hover:underline ${isActive ? "text-green-500" : ""}`
+  }
+>
+  Hosts
+</NavLink>
+
+<NavLink
+  to="/about"
+  className={({ isActive }) =>
+    `hover:underline ${isActive ? "text-green-500" : ""}`
+  }
+>
+  About
+</NavLink>
+
+<NavLink
+  to="/vans"
+  className={({ isActive }) =>
+    `hover:underline ${isActive ? "text-green-500" : ""}`
+  }
+>
+  Vans
+</NavLink>
     </div>
   </div>
   )
--- src/layouts/HostLayout.jsx
+++ src/layouts/HostLayout.jsx
@@ -1,13 +1,21 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 function HostLayout() {
+  const linkClass = ({ isActive }) =>
+  `font-bold hover:underline hover:bg-blue-200 p-2 rounded-md ${
+    isActive ? "text-green-500" : ""
+  }`;
+
+
   return (
     <div>
       <div className=' flex gap-10 '>
-        <Link to='/hosts' className=' font-bold hover:underline hover:bg-blue-200 p-2 rounded-md' >Dashboard</Link>
-        <Link to='/hosts/income'  className='  font-bold hover:underline hover:bg-blue-200 p-2 rounded-md'>Income</Link>
-        <Link to='/hosts/reviews'  className=' font-bold hover:underline hover:bg-blue-200 p-2 rounded-md'>Reviews</Link>
+        <NavLink to='/hosts' 
+          className={linkClass}
+        >Dashboard</NavLink>
+        <NavLink to='/hosts/income'  className={linkClass}>Income</NavLink>
+        <NavLink to='/hosts/reviews'  className={linkClass}>Reviews</NavLink>
       </div>
       <Outlet/>
     </div>
