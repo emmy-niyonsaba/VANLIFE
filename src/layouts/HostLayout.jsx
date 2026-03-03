4ed18e773b452cb51c8f2ac1d52d696f8808f1fb
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-03 09:26:33 +0200

    done with end props in NavLinks

--- src/layouts/HostLayout.jsx
+++ src/layouts/HostLayout.jsx
@@ -3,21 +3,18 @@ import { Outlet } from 'react-router-dom'
 import { NavLink } from 'react-router-dom'
 function HostLayout() {
   const linkClass = ({ isActive }) =>
-  `font-bold hover:underline hover:bg-blue-200 p-2 rounded-md ${
-    isActive ? "text-green-500" : ""
-  }`;
+    `font-bold hover:underline hover:bg-blue-200 p-2 rounded-md ${isActive ? "text-green-500" : ""
+    }`;
 
 
   return (
     <div>
       <div className=' flex gap-10 '>
-        <NavLink to='/hosts' 
-          className={linkClass}
-        >Dashboard</NavLink>
-        <NavLink to='/hosts/income'  className={linkClass}>Income</NavLink>
-        <NavLink to='/hosts/reviews'  className={linkClass}>Reviews</NavLink>
+        <NavLink to='/hosts' className={linkClass} end>Dashboard</NavLink>
+        <NavLink to='/hosts/income' className={linkClass}>Income</NavLink>
+        <NavLink to='/hosts/reviews' className={linkClass}>Reviews</NavLink>
       </div>
-      <Outlet/>
+      <Outlet />
     </div>
   )
 }
