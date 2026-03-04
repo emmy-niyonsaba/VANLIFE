cfe6e8f70120d80f1794c8c80cf522f553944dbf
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-04 16:03:44 +0200

    done by filtering by searchPrams

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
-import { Link,useSearchParams } from 'react-router-dom'
+import { Link, useSearchParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import VansLayout from '../layouts/VansLayout'
 
 const Vans = () => {
     const [vans, setVans] = useState([])
+
     const [searchParams, setSearchParams] = useSearchParams()
-    console.log(searchParams.get('type'));
+    const filterType = searchParams.get('type');
+    const filteredVans = filterType ? vans.filter(van => van.type === filterType) : vans;
+
+    // Categories
     const categories = ["simple", "luxury", "rugged"]
     // console.log(vans)
     useEffect(() => {
@@ -17,23 +21,26 @@ const Vans = () => {
     return (
         <>
             <h1 className=' text-5xl font-bold  m-10'>Explore our van options</h1>
-                <div className='flex gap-4 justify-around m-10'>
-                    {categories.map((category) => {
-                        return (
-                            <button key={category} className=' bg-[#f0ca9c] text-white px-4 py-2 rounded-md'>{category}</button>
-                        )
-                    })}
-                  <Link>  <h2 className=' font-bold text-2xl '>Clear Filter</h2></Link>
-                </div>
-        <div className='flex flex-wrap gap-4 justify-center mt-10'>
-            {
-                vans.map((van) => {
+            <div className='flex gap-4 justify-around m-10'>
+                {categories.map((category) => {
                     return (
-                        <VansLayout key={van.id} {...van} />
+                        <button key={category} className=' bg-[#f0ca9c] text-white px-4 py-2 rounded-md'
+                        onClick={()=>setSearchParams({ type: category })}>{category}</button>
                     )
-                })
-            }
-        </div>
+                })}
+                <Link><h2 className=' font-bold text-2xl '>Clear Filter</h2></Link>
+            </div>
+            <div className='flex flex-wrap gap-4 justify-center mt-10'>
+                {
+
+
+                    filteredVans.map((van) => {
+                        return (
+                            <VansLayout key={van.id} {...van} />
+                        )
+                    })
+                }
+            </div>
         </>
 
     )
