7a62b9763c695312c4e16b79600a67d2564f1597
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-08 18:56:23 +0200

    add the respective comment in detaisl(host)  page

--- src/components/hosts/Details.jsx
+++ src/components/hosts/Details.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { useOutletContext } from 'react-router-dom'
+// The Details component uses the useOutletContext hook to access the van data passed down from the parent route
 function Details() {
   const van = useOutletContext()
   
   return (
+    // The component renders the details of the van, including its name, description, type, and visibility
     <div className=' m-5 flex flex-col gap-5 '>
       <h1 className=' text-2xl font-bold'>Details about the van</h1>
       <p className='text-2xl'><span className=' text-2xl font-bold'>Name:</span>   {van.name}</p>
