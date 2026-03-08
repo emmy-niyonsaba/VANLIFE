4d7e48e5bacd25ff9590512faeff53fae8859a56
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-08 18:54:38 +0200

    add the respective comment in not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
-
+// A simple component to display a 404 Not Found message and a link to go back to the home page
 function NotFound() {
   return (
     <div className="nf-root">
       <h1 className='nf-title'>Sorry — the page you were looking for was not found.</h1>
+      // A link to navigate back to the home page
       <Link to='/' className="nf-btn">Back to Home</Link>
     </div>
   )
