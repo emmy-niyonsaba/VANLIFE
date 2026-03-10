98b9025a127be22e452fa6d578f4980ac88af098
Emmy Niyonsaba <emmmyson5ku@gmail.com>
2026-03-10 09:34:53 +0200

    done by styling the Income static page

--- src/components/hosts/Income.jsx
+++ src/components/hosts/Income.jsx
@@ -2,32 +2,32 @@ import React from "react"
 
 export default function Income() {
     const transactionsData = [
-        { amount: 720, date: "Jan 3, '23", id: "1" },
-        { amount: 560, date: "Dec 12, '22", id: "2" },
-        { amount: 980, date: "Dec 3, '22", id: "3" },
+        { amount: 720, date: "Jan/3/23", id: "1" },
+        { amount: 560, date: "Dec/12/22", id: "2" },
+        { amount: 980, date: "Dec/3/22", id: "3" },
     ]
     return (
-        <section className="host-income">
-            <h1>Income</h1>
-            <p>
-                Last <span>30 days</span>
+        <section className=" flex flex-col gap-10 mt-5">
+            <h1 className="text-2xl font-bold">Income</h1>
+            <p className="text-sm ">
+                Last <span className=" underline font-bold">30 days</span>
             </p>
-            <h2>$2,260</h2>
+            <h2 className=" text-4xl font-bold ">$2,260</h2>
             <img
                 className="graph"
                 src="/assets/images/income-graph.png"
                 alt="Income graph"
             />
-            <div className="info-header">
+            <div className=" flex items-center justify-between">
                 <h3>Your transactions (3)</h3>
-                <p>
-                    Last <span>30 days</span>
+                <p className="text-sm ">
+                    Last <span className=" underline font-bold">30 days</span>
                 </p>
             </div>
-            <div className="transactions">
+            <div className=" flex flex-col gap-10">
                 {transactionsData.map((item) => (
-                    <div key={item.id} className="transaction">
-                        <h3>${item.amount}</h3>
+                    <div key={item.id} className=" flex items-center justify-between">
+                        <h3 className="text-2xl font-bold">${item.amount}</h3>
                         <p>{item.date}</p>
                     </div>
                 ))}
